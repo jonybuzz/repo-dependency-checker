f021b56086aa23d80a1cfa5c4e9231267f3cfc1c
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2019-10-07 17:47:19 -0300

    parseo de nombres de servicios y versiones

--- routes/compose.js
+++ routes/compose.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 const Octokit = require("@octokit/rest");
 var base64 = require('base-64');
+const YAML = require('yaml')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
   const octokit = Octokit({
-    auth: "eb46253c59d32651a49bc7000ff32eda5f57c6a6",
-    userAgent: 'myApp v1.2.3',
+    auth: "c1ad5bf34e80d78eb04f165f95d7fa00b74a0dad",
+    userAgent: 'repo-dependency-checker v1.0.0',
     previews: ['jean-grey', 'symmetra'],
     baseUrl: 'https://api.github.com',
-
     log: {
       debug: () => {},
       info: () => {},
@@ -25,14 +25,26 @@ router.get('/', function(req, res, next) {
     }
   })
 
-  var compose = octokit.repos.getContents({
+  octokit.repos.getContents({
     owner:'lorfinsa',
     repo:'ccc-compose',
     path:'ccc-apis-nr.yml',
     ref:'staging'
   }).then(function(resp){
-    res.send(base64.decode(resp.data.content))
-  })
+    var fileContent = base64.decode(resp.data.content)
+    var yaml = YAML.parse(fileContent)
+    var apis = Object.entries(yaml.services).map(function(entry){
+        var service = entry[1]
+        var imageAndTag = service.image.split('/')[1]
+        var image = imageAndTag.split(':')[0]
+        var tag = imageAndTag.split(':')[1]
+        return {
+          repo: image,
+          version: tag
+        }
+    })
+    res.send(apis)
+  }).catch(err => console.log(err))
 });
 
 module.exports = router;
