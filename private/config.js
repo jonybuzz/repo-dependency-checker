2a6c6a5a66f99cb656f956b50924ee4adcf378c9
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2019-10-31 13:11:40 -0300

    callback url como variable de entorno

--- private/config.js
+++ private/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     application: {
         name: 'repo-dependency-checker',
-        version: 'v1.1.0'
+        version: 'v1.2.0'
     },
     github: {
         api: 'https://api.github.com',
         clientID: process.env.RDC_CLIENT_ID,
         clientSecret: process.env.RDC_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/auth/callback'
+        callbackURL: process.env.RDC_AUTH_CALLBACK_URL
     }
 };
